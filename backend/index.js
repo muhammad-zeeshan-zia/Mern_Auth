bcbfe5d0e73c1a5555fd3724794cf3112379fce4
Muhammad Zeeshan Zia <95034881+zeeshihulk@users.noreply.github.com>
2024-03-17 23:28:38 +0500

    frontend url

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://mern-auth-frontend-olive.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true,
+    }
+));
 app.use(express.json());
 
 // MongoDB Connection
