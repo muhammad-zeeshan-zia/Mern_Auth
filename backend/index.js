9857160ab9f54f82780201a252ddc9aae8458b3f
Muhammad Zeeshan Zia <95034881+zeeshihulk@users.noreply.github.com>
2024-03-18 13:34:34 +0500

    Updated

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,15 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // Middleware to set CORS headers
 
-app.use(cors())
+app.use(cors(
+    {
+    origin:function(origin,callback){
+        return callback(null,true);
+    },
+    optionsSuccessStatus:200,
+    credentials:true,
+    }
+))
 app.use(express.json());
 
 // MongoDB Connection
