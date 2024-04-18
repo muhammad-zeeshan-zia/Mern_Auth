c205c47b7fd50a3eceab618e04e2920ac1d09e5a
Muhammad Zeeshan Zia <95034881+muhammad-zeeshan-zia@users.noreply.github.com>
2024-04-18 19:09:09 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = document.getElementById('root');
+
+// Render your app
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
-
-    <App />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
