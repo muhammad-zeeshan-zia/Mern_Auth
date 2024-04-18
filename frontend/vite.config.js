1a51876c78816b10ccfc7256e32a3e07c4f31781
Muhammad Zeeshan Zia <95034881+muhammad-zeeshan-zia@users.noreply.github.com>
2024-04-18 19:07:06 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
   server: {
 host: true,
 strictPort: true,
