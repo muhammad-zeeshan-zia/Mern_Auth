b039127727068435f8d4107e164fe484fa5b0dbd
Muhammad Zeeshan Zia <95034881+muhammad-zeeshan-zia@users.noreply.github.com>
2024-04-18 18:24:32 +0500

    Update Register.jsx

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -15,7 +15,7 @@
     
 //         try{
 
-//             const res = await axios.post('http://localhost:5000/api/auth/register',
+//             const res = await axios.post('https://booking-back-u1gv.onrender.com/api/auth/register',
 //             {
 //                 username,email,password
 //             }
